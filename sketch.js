36b7daffb3654bb9ef697e1a86fc155a99ff6d6a
erik jensen <fluffy@eriks-MBP-1031.wifi.pcc.edu>
2020-01-09 10:33:32 -0800

    first commit

---
+++ sketch.js
@@ -0,0 +1,58 @@
+var inc=0;
+let drippy = [];
+let bounds = true;
+let numDrips=2000;
+
+function setup() {
+  createCanvas(1300, 1000);
+  for (let i=0;i<numDrips;i++){
+  drippy.push(new drip());
+  }
+}
+
+function draw() {
+  //background(220);
+  for (let i=0;i<drippy.length;i++){
+ drippy[i].update();
+ drippy[i].display();
+  }
+}
+
+class drip{
+  constructor(){
+  this.x = random(width);
+  this.y = 0;
+  this.diamter=10;
+  this.speed=random(1.5,3);
+  this.thresh=0;
+  this.colorFlip=true;
+  }
+  
+  update(){
+    this.speed=abs(random(2,5)+random(-5,.5));
+      if (this.thresh>=numDrips/20){
+       this.colorFlip=!this.colorFlip;
+      //this.x = random(width);
+      this.y=0;
+      this.thresh=0;
+    } else {
+      if (this.y>height){
+      this.thresh++;
+      }
+      this.y+=this.speed;
+    }
+  }
+  
+  display(){
+     noStroke();
+       if (this.colorFlip==true){
+    fill(0);
+  } else {
+    fill(255);
+  }
+    ellipse(this.x,this.y,this.diamter,this.diamter);
+  }
+}
+  
+  
+  
