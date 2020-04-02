d3d21b126f3aadae13778a2a3670c5b45f763419
xmeatballx <59705488+xmeatballx@users.noreply.github.com>
2020-04-01 21:52:20 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,15 +6,15 @@ let numDrips=2000;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   for (let i=0;i<numDrips;i++){
-  drippy.push(new drip());
+    drippy.push(new drip());
   }
 }
 
 function draw() {
   //background(220);
   for (let i=0;i<drippy.length;i++){
- drippy[i].update();
- drippy[i].display();
+    drippy[i].update();
+    drippy[i].display();
   }
 }
 
@@ -32,29 +32,29 @@ class drip{
     this.speed=abs(random(2,5)+random(-5,.5));
       if (this.thresh>=numDrips/20){
        this.colorFlip=!this.colorFlip;
-      //this.x = random(width);
-      this.y=0;
-      this.thresh=0;
+        //this.x = random(width);
+        this.y=0;
+        this.thresh=0;
     } else {
-      if (this.y>height){
-      this.thresh++;
-      }
-      this.y+=this.speed;
+       if (this.y>height){
+        this.thresh++;
+       }
+       this.y+=this.speed;
     }
   }
   
   display(){
      noStroke();
-       if (this.colorFlip==true){
-    fill(0);
-  } else {
-    fill(255);
-  }
-    ellipse(this.x,this.y,this.diamter,this.diamter);
-    fill(255);
-    textSize(72);
-    textAlign(CENTER);
-    text("DRIPPPPPPPP",width/2,height/2);
+     if (this.colorFlip==true){
+      fill(0);
+     } else {
+      fill(255);
+     }
+     ellipse(this.x,this.y,this.diamter,this.diamter);
+     fill(255);
+     textSize(48);
+     textAlign(CENTER);
+     text("DRIPPPPPPPP",width/2,height/2);
   }
 }
   
